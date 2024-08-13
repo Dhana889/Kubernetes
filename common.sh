653e6ab04beac7cb579e422982fda6a3b4564d26
Dhana <31102690+Dhana889@users.noreply.github.com>
2024-08-13 20:08:41 +0530

    Update common.sh

--- common.sh
+++ common.sh
@@ -13,9 +13,27 @@ sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 # keeps the swaf off during reboot
 sudo apt-get update -y
 
+# Configure persistent loading of modules
+sudo tee /etc/modules-load.d/k8s.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+# Ensure you load kernel modules
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# (optional) if br_netfilter module is not updated
+# sudo apt install ebtables -y
+
+# lsmod | grep overlay
+# lsmod | grep br_netfilter
+
 # To manually enable IPv4 packet forwarding:
 # sysctl params required by setup, params persist across reboots
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
 
@@ -24,6 +42,30 @@ sudo sysctl --system
 
 # Verify that net.ipv4.ip_forward is set to 1 with:
 sysctl net.ipv4.ip_forward
+
+# update the package repository and download all the necessary packages
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+# Download and install keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
+
+# Add CRI-O repo
+curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key|sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+
+sudo chmod a+r /etc/apt/keyrings/cri-o-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /"|sudo tee /etc/apt/sources.list.d/cri-o.list
+
+# Install CRI-O
+sudo apt update
+sudo apt install cri-o
+
+# Start and enable Service
+sudo systemctl daemon-reload
+sudo systemctl restart crio
+sudo systemctl enable crio
+sudo systemctl status crio
 
 # Update the repo and download all the required packages
 sudo apt-get update
@@ -39,43 +81,3 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-
-# Installing CRI-O runtime
-# Configure persistent loading of modules
-sudo tee /etc/modules-load.d/k8s.conf <<EOF
-overlay
-br_netfilter
-EOF
-
-# Ensure you load modules
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
-# Set up required sysctl params
-sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
-EOF
-
-# Reload sysctl
-sudo sysctl --system
-
-# Add Cri-o repo
-curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key|sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /"|sudo tee /etc/apt/sources.list.d/cri-o.list
-
-# Install CRI-O
-sudo apt update
-sudo apt install cri-o
-
-# Start and enable Service
-sudo systemctl daemon-reload
-sudo systemctl restart crio
-sudo systemctl enable crio
-sudo systemctl status crio
-
-
-# Need to join the worker node to the master node using the Kubeadm join command
-
-echo "If this is a Worker node, then run the join command"
